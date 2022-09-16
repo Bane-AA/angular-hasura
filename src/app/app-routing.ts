80c55a3a49fea9baf13ae1ee368c32b864b05125
Bane-AA <bane@angularadvisors.com>
2022-09-16 14:37:16 +0200

    change to stand alone components

---
+++ src/app/app-routing.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+
+export const APP_ROUTES: Routes = [
+  {
+      path: '',
+      pathMatch: 'full',
+      redirectTo: 'home'
+  },
+  {
+      path: 'home',
+      component: HomeComponent
+  },
+  // {
+  //     path: 'flight-booking',
+  //     loadChildren: () =>
+  //         import('./booking/flight-booking.routes').then(m => m.FLIGHT_BOOKING_ROUTES)
+  // },
+  // {
+  //     path: 'next-flight',
+  //     loadComponent: () =>
+  //         import('./next-flight/next-flight.component').then(m => m.NextFlightComponent)
+  // },
+  // {
+  //     path: 'about',
+  //     loadComponent: () =>
+  //         import('./about/about.component').then(m => m.AboutComponent)
+  // },
+];
